1208dc89343bed797add16cc09d41968a7a170ba
Lucas Gallo <lucasgallo5046@gmail.com>
2023-02-15 20:51:56 -0300

    finalização o estilo do CSS

--- src/pages/Contact/index.js
+++ src/pages/Contact/index.js
@@ -5,32 +5,45 @@ import { useState } from 'react';
 const Contact = () => {
 
     const[data, setData] = useState( {  } )
+
     const submitFormData = (e) => {
         e.preventDefault()
         console.log(data)
+        alert("DEU CERTO")
     }
     
     return(
-        <div>
-            <Navbar />
-            <h1>Contato</h1>
+        <div className="container">
+            <Navbar className="navbar"/>
+            <h1 className="title">Contato</h1>
             <form onSubmit={submitFormData}>
                 <input 
                     type='text' 
                     placeholder='Nome'
                     onChange={ e => setData( { ...data, nome: e.target.value } ) }
+                    
                 />
                 <input 
                     type='text' 
                     placeholder='Email'
                     onChange={ e => setData( { ...data, email: e.target.value } ) }
+                    
                 />
                 <input 
                     type='text' 
                     placeholder='Telefone' 
                     onChange={e => setData( { ...data, telefone: e.target.value } ) }
+                    
                 />
+
+                <textarea
+                    placeholder="Digite sua mensagem..."
+                    onChange={ e => setData( { ...data, mensagem: e.target.value } ) }
+                    
+                />
+
                 <input 
+                    className="button"
                     type='submit' 
                     value='Cadastrar' 
                 />
